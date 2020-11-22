d5b65e1c596c0db86f664840253e5d88fba86b73
igor <kzvigor@gmail.com>
2020-11-22 21:45:05 +0300

    add new functions

---
+++ js/modules/timer.js
@@ -0,0 +1,44 @@
+function timer (id, deadline) {
+
+    function getTimeRemaing(endtime) {
+        const total = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(total / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((total / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((total / (1000 * 60)) % (60)),
+            seconds = Math.floor((total / 1000) % (60));
+        return {
+            total,
+            days,
+            hours,
+            minutes,
+            seconds
+        };
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaing(endtime);
+            days.textContent = t.days;
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+            if (t <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+    }
+
+    setClock(id, deadline);
+}
+
+export default timer;
